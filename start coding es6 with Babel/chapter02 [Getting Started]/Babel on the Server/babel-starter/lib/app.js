5c169764d5c162f5ec49e10810fb22c7f795669f
ahmed-hamdy90 <ahmedhamdy20@gmail.com>
2016-01-25 22:25:36 +0200

    Babel on the Server Video: create babel-starter app include node server file, server.js file contain ES6 features tranform using Babel

---
+++ start coding es6 with Babel/chapter02 [Getting Started]/Babel on the Server/babel-starter/lib/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+// create express app
+var app     = express();
+// create server using express app
+var server  = require('http').createServer(app);
+
+// make root Route for app to read index.html file under dist Folder
+app
+  .use('/', express.static(__dirname + '/../dist', { maxAge: 1000000 }));
+
+// make server listen on 9000 port
+server
+  .listen(9000, () => console.log('App Strated'));
