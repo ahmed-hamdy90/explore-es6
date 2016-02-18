a853895d88274649f6ebd0fd44a9534fa42a9d63
ahmed-hamdy90 <ahmedhamdy20@gmail.com>
2016-02-18 17:51:01 +0200

    begin to write fibanacci method

--- start coding es6 with Babel/chapter03 (Experimenting With ES6)/Generators/babel-starter/portable/index.js
+++ start coding es6 with Babel/chapter03 (Experimenting With ES6)/Generators/babel-starter/portable/index.js
@@ -11,8 +11,19 @@ const ID_PREFIX = 'USER_';
  * validate Id value that it's type is string
  * and begin with ID_PREFIX constant value
  * @param  {*} id  id value
- * @return {boolean} result of id validation 
+ * @return {boolean} result of id validation
  */
 function validateId(id) {
 	return typeof id === 'string' && id.indexOf(ID_PREFIX) === 0;
-} 
+}
+
+/**
+ * fibonacci method is a Generator function to apply Fibonacci Sequence
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*} for function* declaration
+ * @see {@link https://en.wikipedia.org/wiki/Fibonacci_number} for define Fibonacci Squance
+ * @return {Generator} Generator Object
+ */
+function* fibonacci() {
+	var n1 = 0;
+	var n2 = 1;	
+}
